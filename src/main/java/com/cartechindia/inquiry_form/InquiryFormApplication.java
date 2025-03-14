9a48fcb9814c32c1eb03a3870dc0ce2765a97214
Shashi <shashinarote97@gmail.com>
2025-03-14 10:23:50 +0530

    Initialized git into project

---
+++ src/main/java/com/cartechindia/inquiry_form/InquiryFormApplication.java
@@ -0,0 +1,13 @@
+package com.cartechindia.inquiry_form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquiryFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquiryFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cartechindia/inquiry_form/InquiryFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartechindia.inquiry_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquiryFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
